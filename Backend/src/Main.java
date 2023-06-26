50482e4ae0bcd6bbff0df973ae21da5b30dd4eab
KOLMTRIIPU2020 <118007503+KOLMTRIIPU2020@users.noreply.github.com>
2023-06-26 20:18:46 +0400

    Update Main.java

--- Backend/src/Main.java
+++ Backend/src/Main.java
@@ -2,22 +2,28 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[5];
+        int[] numbers = new int[100];
         Random random = new Random();
-
-        // Заполняем массив случайными значениями
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(100); // Генерируем случайное число от 0 до 99
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
         }
-
-        // Находим среднюю арифметическую всех элементов
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+        
+        double average = 0;
+        for (int number : numbers) {
+            average += number;
         }
-        double average = (double) sum / array.length;
-
+        average /= numbers.length;
+        
+        int targetNumber = numbers[random.nextInt(numbers.length)];
+        int index = -1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == targetNumber) {
+                index = i;
+                break;
+            }
+        }
+        
         System.out.println("Средняя арифметическая: " + average);
-
+        System.out.println("Порядковый номер числа " + targetNumber + ": " + index);
     }
 }
